374d512295e25507a7c541f18def591b2856a1c1
yosssi <yoshida.keiji.84@gmail.com>
2014-08-01 19:14:57 +0900

    Add render.go

---
+++ render.go
@@ -0,0 +1,71 @@
+package acerender
+
+import (
+	"bytes"
+	"io"
+	"net/http"
+	"strings"
+
+	"github.com/go-martini/martini"
+	"github.com/martini-contrib/render"
+	"github.com/yosssi/ace"
+)
+
+const defaultContentType = render.ContentHTML + "; charset=utf-8"
+
+// Render is an interface for parsing Ace templates and redering HTML.
+type Render interface {
+	HTML(status int, name string, v interface{}, htmlOpt ...render.HTMLOptions)
+}
+
+// render represents a renderer of Ace templates.
+type renderer struct {
+	http.ResponseWriter
+	req  *http.Request
+	opts *ace.Options
+}
+
+// HTML parses the Ace templates and renders HTML to the response writer.
+func (r *renderer) HTML(status int, name string, v interface{}, htmlOpt ...render.HTMLOptions) {
+	var basePath, innerPath string
+
+	paths := strings.Split(name, ":")
+
+	basePath = paths[0]
+
+	if len(paths) > 1 {
+		innerPath = paths[1]
+	}
+
+	tpl, err := ace.ParseFiles(basePath, innerPath, r.opts)
+
+	if err != nil {
+		http.Error(r, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	buf := new(bytes.Buffer)
+
+	if err := tpl.Execute(buf, v); err != nil {
+		http.Error(r, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	r.Header().Set(render.ContentType, defaultContentType)
+	r.WriteHeader(status)
+	io.Copy(r, buf)
+}
+
+// Renderer is a Martini middleware that maps a render.Render service into the Martini handler chain.
+func Renderer(opts *ace.Options) martini.Handler {
+	return func(res http.ResponseWriter, req *http.Request, c martini.Context) {
+		c.MapTo(
+			&renderer{
+				ResponseWriter: res,
+				req:            req,
+				opts:           opts,
+			},
+			(*Render)(nil),
+		)
+	}
+}
